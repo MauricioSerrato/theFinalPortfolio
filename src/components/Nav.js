01320fbe5431864e285be5cf5695e512d38bf7b9
MauricioSerrato <mserato99@gmail.com>
2022-09-22 20:31:50 -0500

    somethign working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,7 @@
-
+import * as React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 
 const Topbar = ({setProjects, setHome, setIntro, setAbout, setBar, setContact}) => {
@@ -39,7 +42,23 @@ const openContacts = () => {
 
 
   return (
-    <></>
+    <Navbar bg="" variant="dark" sticky=""  className=''>
+    <Container >
+      <div>
+      <Navbar.Brand >
+        <img className='logo' src='./logo.png' alt='logo'/>
+      </Navbar.Brand>
+      </div>
+      <div>
+      <Nav className="me-auto">
+        <Nav.Link className="navItems" onClick={() => {openHome()}} >Home</Nav.Link>
+        <Nav.Link className="navItems" onClick={() => {getAbout()}} >About</Nav.Link>
+        <Nav.Link className="navItems" onClick={()=> {openProjects()}} >Projects</Nav.Link>
+        <Nav.Link className="navItems" onClick={() => {openContacts()}} >Contact</Nav.Link>
+      </Nav>
+      </div>
+    </Container>
+  </Navbar>
 
   )
 }
