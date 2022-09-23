2a096f037ae3ad7ad0e110162a06a40e30438635
MauricioSerrato <mserato99@gmail.com>
2022-09-23 11:03:00 -0500

    added routes to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,8 +11,12 @@ const Projects = () => {
         <Card.Text>
             BraveHeart is a full stack application that allows its users to keep track of their progressive overload during workouts, have a variety of workouts to choose from (with tutorials), target specific areas to train, while also providign other features like healthy foods, and in app virtual meetings. This appilcation was built using Node.js, Mongoose, Express, Ejs, Bootstrap, Html, and Css. 
         </Card.Text>
+        <a href="https://www.google.com/url?q=https://infinite-escarpment-98027.herokuapp.com/home&sa=D&source=editors&ust=1663951725254830&usg=AOvVaw09SCKxSxtcV8Lhc33uX4FS" target="_blank" rel='noreferrer'>
         <Button  className="btn">Visit Site</Button>
+        </a>
+        <a href='https://github.com/MauricioSerrato/BraveHeart'>
         <Button className="btn">Visit Git Repo</Button>
+        </a>
 
       </Card.Body>
     </Card>
@@ -24,8 +28,12 @@ const Projects = () => {
         <Card.Text>
             Comic World is a Full Stack Application built around the interest for comics and superheros that allows its users to have all CRUD (create, remove, update, and delete) comics. This Group project was built usign the MERN stack: Node.js, Mongoose, Express, and React.js. 
         </Card.Text>
+        <a href='https://fierce-stream-03665.herokuapp.com'target="_blank" rel='noreferrer'>
         <Button  className="btn">Visit Site</Button>
+        </a>
+        <a href='https://github.com/moodzter/Front_End_Marvel'target="_blank" rel='noreferrer'>
         <Button className="btn">Visit Git Repo</Button>
+        </a>
       </Card.Body>
     </Card>
 
@@ -36,12 +44,14 @@ const Projects = () => {
         <Card.Text>
           Yello! An application built as a social media platform. This group project was created in the intent for its users to express their happy times to other users. It is a fullt stack application usign Django API server as a backend and React as a Front End. This application was created with multiple model to model realtions if the back end so that each post had multiple comments. This application has full CRUD funcationality.
         </Card.Text>
+        <a href="https://boiling-lake-14034.herokuapp.com/login" target="_blank" rel='noreferrer'>
         <Button  className="btn">Visit Site</Button>
+        </a>
+        <a href="https://github.com/amiemora/yello_backend_2" target="_blank" rel='noreferrer'>
         <Button className="btn">Visit Git Repo</Button>
+        </a>
       </Card.Body>
     </Card>
-
-   
     </>
   );
 }
