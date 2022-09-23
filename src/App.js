ff856b71e81f6e9c0b5eed0a2685233318910a4b
MauricioSerrato <mserato99@gmail.com>
2022-09-23 10:03:57 -0500

    imported css and nav

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {useState} from 'react';
 import Home from './components/Home'
 import Contact from './components/Contact'
 import About from './components/About'
+import './App.css'
 
 function App() {
 
