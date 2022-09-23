9d7a5cb19ae041ba8e5ebaaf728133517518aee6
MauricioSerrato <mserato99@gmail.com>
2022-09-22 20:16:40 -0500

    removed bootstrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+
 import {FaLinkedin} from 'react-icons/fa'
 import {FaGithub} from 'react-icons/fa';
 import {MdEmail} from 'react-icons/md'
@@ -13,28 +12,29 @@ const Footer = ({setProjects, setHome, setIntro, setAbout, setBar, bar, setConta
     setAbout(false)
   }
   return (
-    <Navbar  className="bottom" sticky="">
-      <Container>
-        <Navbar.Brand className="occupation"> Software Engineer</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end"> 
-          <Navbar.Text >
-            <a href='https://www.linkedin.com/in/mauricioserrato/' target="_blank" rel='noreferrer' >
-            <FaLinkedin id="LinkedIn"/>
-            </a>
-          </Navbar.Text>
-          <Navbar.Text>
-            <a href="https://github.com/MauricioSerrato" target="_blank" rel='noreferrer'>
-              <FaGithub id="Git"/>
-            </a>  
-          </Navbar.Text>
-          <Navbar.Text>
-              <MdEmail  onClick={openContacts} id="email"/>
+    <></>
+    // <Navbar  className="bottom" sticky="">
+    //   <Container>
+    //     <Navbar.Brand className="occupation"> Software Engineer</Navbar.Brand>
+    //     <Navbar.Toggle />
+    //     <Navbar.Collapse className="justify-content-end"> 
+    //       <Navbar.Text >
+    //         <a href='https://www.linkedin.com/in/mauricioserrato/' target="_blank" rel='noreferrer' >
+    //         <FaLinkedin id="LinkedIn"/>
+    //         </a>
+    //       </Navbar.Text>
+    //       <Navbar.Text>
+    //         <a href="https://github.com/MauricioSerrato" target="_blank" rel='noreferrer'>
+    //           <FaGithub id="Git"/>
+    //         </a>  
+    //       </Navbar.Text>
+    //       <Navbar.Text>
+    //           <MdEmail  onClick={openContacts} id="email"/>
              
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    //       </Navbar.Text>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 }
 
