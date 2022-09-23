ebcf1ea1280c54fa23c81a6f4b26ea50586aaea1
MauricioSerrato <mserato99@gmail.com>
2022-09-23 11:12:00 -0500

    added react icons might break heroku

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,10 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa';
+import {MdEmail} from 'react-icons/md'
 
-
-
-
-const Footer = ({setProjects, setHome, setIntro, setAbout, setBar, bar, setContact}) => {
+const Footer = ({setProjects, setHome, setIntro, setAbout, setContact}) => {
   const openContacts = () => {
     setContact(true)
     setHome(false)
@@ -10,7 +12,28 @@ const Footer = ({setProjects, setHome, setIntro, setAbout, setBar, bar, setConta
     setAbout(false)
   }
   return (
-    <></>
+    <Navbar  className="bottom" sticky="">
+      <Container>
+        <Navbar.Brand className="occupation"> Software Engineer</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end"> 
+          <Navbar.Text >
+            <a href='https://www.linkedin.com/in/mauricioserrato/' target="_blank" rel='noreferrer' >
+            <FaLinkedin id="LinkedIn"/>
+            </a>
+          </Navbar.Text>
+          <Navbar.Text>
+            <a href="https://github.com/MauricioSerrato" target="_blank" rel='noreferrer'>
+              <FaGithub id="Git"/>
+            </a>  
+          </Navbar.Text>
+          <Navbar.Text>
+              <MdEmail  onClick={openContacts} id="email"/>
+             
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
